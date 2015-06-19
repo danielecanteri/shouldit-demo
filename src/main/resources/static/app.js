5359f0affd4eb4187545a8bc37fd4bf0d465606a
daniele.canteri <daniele.canteri@gmail.com>
2015-06-19 15:27:33 +0200

    elenco todo

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,3 +1,13 @@
-angular.module('todoApp', []).controller('TodoController', function($scope) {
-	$scope.what = 'Angularjs';
-});
+angular.module('todoApp', []).controller(
+		'TodoController',
+		function($scope, $http) {
+			$scope.load = function() {
+				$scope.loading = true;
+				$http.get('/api/todo/list').success(function(data) {
+					$scope.todos = data;
+					$scope.loading = false;
+				})
+			}
+
+			$scope.load();
+		});
