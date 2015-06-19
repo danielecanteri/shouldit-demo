da5218ca22d3d10639f857f2cfe7b169b68f446f
daniele.canteri <daniele.canteri@gmail.com>
2015-06-19 16:05:31 +0200

    test selenium applicazione

---
+++ src/test/java/demo/HomeTest.java
@@ -0,0 +1,18 @@
+package demo;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class HomeTest {
+
+	@Test
+	public void ilTitoloDeveEssereTodo() {
+		WebDriver driver = new HtmlUnitDriver();
+		driver.get("http://localhost:8080/index.html");
+		Assert.assertThat(driver.getTitle(), is("Todo"));
+	}
+}
