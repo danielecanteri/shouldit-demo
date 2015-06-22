8635a2deecd5f8815183d0d068de10bdf20e9547
daniele.canteri <daniele.canteri@gmail.com>
2015-06-22 21:46:55 +0200

    integrazione test protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,13 @@
+var jasmineReporters = require('jasmine-reporters');
 exports.config = {
+  framework: 'jasmine2',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['src/test/javascript/**/*-protractor.js']
+  specs: ['src/test/javascript/**/*-protractor.js'],
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter(
+      {
+        savePath: 'target'
+      }
+    ));
+  }
 };
